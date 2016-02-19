80a05ecbf69d44197e130db1ad721b70ddfefa0c
James Carscadden <james@carscadden.org>
2016-02-19 14:42:25 -0500

    initial commit

---
+++ bundle.js
@@ -0,0 +1,21 @@
+var path = require('path');
+var tl = require('vso-task-lib');
+
+var bundle = new tl.ToolRunner(tl.which('bundle', true))
+bundle.arg('install --deployment')
+
+var path = tl.getInput('path', false);
+
+if (path) {
+    bundle.arg('--path=' + path)
+}
+
+bundle.exec({ failOnStdErr: false})
+.then(function(code) {
+    tl.exit(code);
+})
+.fail(function(err) {
+    console.error(err.message);
+    tl.debug('taskRunner fail');
+    tl.exit(1);
+})
