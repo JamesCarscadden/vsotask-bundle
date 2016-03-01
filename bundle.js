2d5db548235c7b86a97ad659b478e7f12f556727
James Carscadden <james@carscadden.org>
2016-03-01 10:34:53 -0500

    add the ability to generate binstubs

--- bundle.js
+++ bundle.js
@@ -1,13 +1,18 @@
 var path = require('path');
 var tl = require('vso-task-lib');
 
-var bundle = new tl.ToolRunner(tl.which('bundle', true))
-bundle.arg('install --deployment')
+var bundle = new tl.ToolRunner(tl.which('bundle', true));
+var binstubs = tl.getInput('createBinstubs', false);
 
+bundle.arg('install --deployment');
 var path = tl.getInput('path', false);
 
 if (path) {
     bundle.arg('--path=' + path)
+}
+
+if (binstubs) {
+    bundle.arg('--binstubs=bin')
 }
 
 bundle.exec({ failOnStdErr: false})
